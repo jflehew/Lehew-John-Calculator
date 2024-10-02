380c1bbbe65a8ba4368f64a871f79b816d04bbb8
jflehew <Johnflehew@gmail.com>
2024-10-02 11:37:28 -0600

    update

---
+++ static/script/script.js
@@ -0,0 +1,20 @@
+let valuesArray = [];
+let opsArray = [];
+
+
+function press(value){
+    valuesArray.push(value);
+}
+
+function setOP(){
+
+}
+
+function clr(element){
+    display.innerText = "0"
+
+}
+
+function calcalculate(){
+
+}
